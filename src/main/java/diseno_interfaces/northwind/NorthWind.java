c2699549ce6917c8728fa26b135500c97ce24dfa
alu <alu@PC63>
2023-11-08 10:10:01 +0100

    erh

---
+++ src/main/java/diseno_interfaces/northwind/NorthWind.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package diseno_interfaces.northwind;
+
+/**
+ *
+ * @author alu
+ */
+public class NorthWind {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
